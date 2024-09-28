d9917f38708cf03d8f7813bcc359eb965146c4de
zijunye <zijunye001@gmail.com>
2024-09-28 12:41:36 -0400

    push

--- frontend/src/app/component/SecondMain.js
+++ frontend/src/app/component/SecondMain.js
@@ -16,25 +16,26 @@ export default function SecondMain({ name, onSubmit }) {
   return (
     <div className="border-black border-8 rounded-xl grid items-center justify-items-center min-h-screen relative p-8">
   
-      <div className="relative mb-44">
-      <div className="font-pixelify text-8xl text-black flex items-center mb-20">
+      <div className="relative">
+        </div>
+      <div className="font-pixelify text-8xl text-black flex items-center ">
           <h1>Hello, {name}</h1>
           <img src="/images/smileChat.png" alt="smile" className="ml-2" />
         </div>
-      <div className="box bg-white border-black border-8 rounded-md shadow-md px-44 py-8 inline-block text-center relative">
+      <div className="box bg-white border-black border-8 rounded-md shadow-md px-80 py-8 inline-block text-center relative">
         <img
           src="/images/plantGrowth.png"
           className="absolute top-[-90px] left-1/2 transform -translate-x-1/2"
           alt="Plant Growth"
         />
         <textarea
-          className="text-black text-lg font-pixelify text-center outline-none py-2 px-4 resize-none rounded-md w-full h-32"
+          className="text-black text-lg font-pixelify text-center outline-none py-2 px-14 resize-none rounded-md w-full h-32"
           placeholder="Let me know what kind of aspect you want to improve yourself?"
           value={textareaInput}
           onChange={handleChange}
           onKeyDown={handleKeyDown} 
         />
-      </div>
+
     </div>
 
 
